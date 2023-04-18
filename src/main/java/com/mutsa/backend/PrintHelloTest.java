1567b4bae9ba923de6e634572ba8cadea05092a1
임규희 <im_gyuh2@imgyuhuiui-MacBookPro.local>
2023-04-19 02:39:02 +0900

    update gitignore

--- src/main/java/com/mutsa/backend/PrintHelloTest.java
+++ src/main/java/com/mutsa/backend/PrintHelloTest.java
@@ -2,7 +2,7 @@ package com.mutsa.backend;
 
 public class PrintHelloTest {
     public static void main(String[] args) {
-        PrintHello printHello = new PrintHello();  // 클래스를 인스턴스화
+        PrintHello printHello = new PrintHello();  // 클래스 인스턴스화
         printHello.print();
     } // psvm 단축어
 }
